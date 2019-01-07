e75af317cf88ac89e61dbe368a332d6862960ae1
Yongwei Dou <yongwei.dou@gmail.com>
2019-01-07 20:52:22 +0900

    Add min and max functions.

---
+++ sort/compare.go
@@ -0,0 +1,17 @@
+package sort
+
+//Min find the minimum from the two integer.
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//Max find the maximum from the tow integer
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
